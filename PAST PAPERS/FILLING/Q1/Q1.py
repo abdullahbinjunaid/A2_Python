0de934f4167e183d3650dab96a7914a405ef7c71
abdullahbinjunaid <abdullah.b.j2007@gmail.com>
2026-04-05 15:09:07 +0500

    commit

--- PAST PAPERS/FILLING/Q1/Q1.py
+++ PAST PAPERS/FILLING/Q1/Q1.py
@@ -1,5 +1,5 @@
+# these 3 lines of code, connect you file to the path of the folder, helpful in bigger projects. 
 import os
-
 current_dir = os.path.dirname(__file__)
 file_path = os.path.join(current_dir, "Data.txt")
 
